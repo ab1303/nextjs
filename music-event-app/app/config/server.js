7570be92ad27c9dc52cab3d84d5b39ff1e63a637
ABDUL WAHAB SHAHID <ab.1303@gmail.com>
2021-09-17 13:29:46 +1000

    Project Structure with Strapi

---
+++ music-event-app/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6971f605504c646f79d24a71f12f2ac6'),
+    },
+  },
+});
